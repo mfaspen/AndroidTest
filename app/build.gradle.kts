e62c0b1b0bed3f64893921f1ca668f70498bf2fb
ShuDan_X <5247737@qq.com>
2025-12-04 17:16:05 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,30 +1,17 @@
 plugins {
-    id("com.android.library")
-    //alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.library") version "7.0.3"
+    id("org.jetbrains.kotlin.android") version "1.5.31"
 }
+
 android {
     namespace = "com.example.Application"
-    compileSdk = 33
+    compileSdk = 29
 
     defaultConfig {
-        //applicationId = "com.example.myappTest"
         minSdk = 29
-        //targetSdk = 33
-        //versionCode = 1
-        //versionName = "1.0"
-
-//        // 这里很关键：告诉Gradle要编译本地C++库
-//        externalNativeBuild {
-//            cmake {
-//                cppFlags += listOf("-std=c++17", "-Wall")
-//            }
-//        }
 
         ndk {
-            abiFilters += listOf("arm64-v8a"
-                //, "armeabi-v7a"
-            )
+            abiFilters.add("arm64-v8a")
         }
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -41,14 +28,13 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 
-    // 启用CMake构建
     externalNativeBuild {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
@@ -62,7 +48,6 @@ android {
 
     sourceSets {
         getByName("main") {
-            // 告诉 Gradle JNI 库的源码（.so 文件）在哪里
             jniLibs.srcDir("libs")
         }
     }
@@ -79,26 +64,15 @@ configurations.all {
 dependencies {
     compileOnly(files("libs/classes.jar")) // <--- 添加这行
     implementation("androidx.activity:activity:1.7.2")
-    implementation ("com.google.android.material:material:1.8.0")
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.google.android.material:material:1.6.1")
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
-//afterEvaluate {
-//    tasks.named("assembleRelease").configure {
-//        doLast {
-//            copy {
-//                from("build/outputs/aar/MyPlugin-release.aar")
-//                into("D:/UnityAARs/")  // 👈 你指定的目标路径
-//            }
-//        }
-//    }
-//}
 
 
 tasks.register<Copy>("copyAar") {
@@ -106,18 +80,13 @@ tasks.register<Copy>("copyAar") {
 
     // 1. 查找源文件：默认的输出目录
     from(fileTree(mapOf(
-        "dir" to "$buildDir/outputs/aar",
+        "dir" to "$build/Diroutputs/aar",
         "include" to sourceAarFileName
     )))
 
-    // 2. 使用 into() 设置目标目录
-    // rootDir 是对项目根目录的引用
     into("D:\\UNITY\\TESTShader\\Screen\\Assets\\Plugins\\Android")
 
-    // 确保目标目录存在 (使用 File.mkdirs() 或 Kotlin 的 .mkdirs())
-    //    doFirst {
-    //        rootDir.resolve("artifacts").mkdirs()
-    //    }
+
 }
 
 // 确保 copyAar 任务在 assembleRelease 任务完成后运行
